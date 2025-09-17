7232734969990ca046f0dbb5e3f98d4db58e3232
robinristo78 <robinristo@gmail.com>
2025-09-17 13:02:31 +0300

    7.1. Added Delete and Edit buttons into the admin view 'getArticleBySlug' pages

--- joga_mysql_oop/routers/admin/article.js
+++ joga_mysql_oop/routers/admin/article.js
@@ -14,10 +14,10 @@ router.post('/article/create', (req, res) => adminArticleController.createNewArt
 // View single article (admin detail)
 router.get('/article/:slug', (req, res) => adminArticleController.getArticleBySlug(req, res));
 
-router.get('/article/update/:id', (req, res) => adminArticleController.getEditArticle(req, res));
+router.get('/article/edit/:id', (req, res) => adminArticleController.getEditArticle(req, res));
 
 // Update article (better: use id)
-router.post('/article/update/:id', (req, res) => adminArticleController.updateArticle(req, res));
+router.post('/article/edit/:id', (req, res) => adminArticleController.updateArticle(req, res));
 
 // Delete article (use POST since no method override)
 router.post('/article/delete/:id', (req, res) => adminArticleController.deleteArticle(req, res));
