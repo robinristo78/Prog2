8a9290c93e3b610f0b96233d923825d155125041
robinristo78 <robinristo@gmail.com>
2025-09-18 13:28:43 +0300

    1.1. Created connection to database

--- joga_sequelize/index.js
+++ joga_sequelize/index.js
@@ -5,6 +5,15 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+sequelize.authenticate().then(() => {
+   console.log('Connected to the database.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World!' });
 });
