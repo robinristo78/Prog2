bf69656dd4154764933d3f34abcdf7ef8ea64044
robinristo78 <robinristo@gmail.com>
2025-10-10 09:58:21 +0300

    4. web_shop_sequelize: Create carts table and manage carts data

--- web_shop_sequelize/controllers/shop.js
+++ web_shop_sequelize/controllers/shop.js
@@ -19,6 +19,56 @@ class shopController {
             products: cartProducts
         });
     }
+
+    async addToCart(req, res) {
+        try {
+            const productId = req.params.productId; //req.body.productId
+            const userCart = await req.user.getCart();
+            const products = await userCart.getProducts({ where: { id: productId } });
+            let product;
+            if (products.length > 0) {
+                product = products[0];
+                let quantity = product.cartItem.quantity;
+                await product.cartItem.update({ quantity: quantity + 1});
+            } else {
+                product = await Product.findByPk(productId);
+                if (!product) return res.status(404).json({ error: "Product not found" });
+                await userCart.addProduct(product, { through: { quantity: 1 } });
+            }
+            res.status(200).json({ message: "Product added to cart successfully" });
+        } catch (error) {
+            console.log("Error adding product to cart: ", error)
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+
+    async removeFromCart(req, res) {
+        try {
+            const productId = req.params.productId; //req.body.productId
+            const userCart = await req.user.getCart();
+
+            if (!userCart) {
+                return res.status(404).json({ error: "Cart not found" });
+            }
+
+            const products = await userCart.getProducts({ where: { id: productId } });
+            if (products.length === 0) {
+                return res.status(404).json({ error: "Product not in cart" });
+            }
+            const product = products[0];
+
+            if (product.cartItem.quantity > 1) {
+                await product.cartItem.update({ quantity: product.cartItem.quantity - 1 });
+            } else {
+                await userCart.removeProduct(product);
+            }
+
+            res.status(200).json({ message: "Product removed from cart successfully" });
+        } catch (error) {
+            console.log("Error removing product from cart: ", error)
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
 }
 
 module.exports = new shopController();
--- web_shop_sequelize/routes/shop.js
+++ web_shop_sequelize/routes/shop.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const shopController = require('../controllers/shop');
 
+// get Cart
 router.get('/cart', (req, res) => shopController.getCart(req, res));
+
+// post add a product to Cart
+router.post("/cart/add/:productId", (req, res) => shopController.addToCart(req, res));
+
+// post remove a product from Cart
+router.post("/cart/remove/:productId", (req, res) => shopController.removeFromCart(req, res));
 
 module.exports = router;
